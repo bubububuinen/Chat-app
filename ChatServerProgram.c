3e50ed1d297bef2fa726db2a4f766af74be31879
BubuinenBubu <144718238+bubububuinen@users.noreply.github.com>
2023-10-28 18:38:11 +0300

    Create ChatServerProgram.c

---
+++ ChatServerProgram.c
@@ -0,0 +1,55 @@
+﻿using SocketFrm;
+using SocketServerApp.Authentication;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketServerApp
+{
+    public class ChatServerProgram
+    {
+        static void Main(string[] args)
+        {
+            IServerUINotifier serverUINotifier = new ConsoleNotifier();
+            Task tcpListenerStartTask = StartTCPListener(serverUINotifier);
+            tcpListenerStartTask.Wait();
+        }
+
+        /// <summary>
+        /// Returns a task that can be awaited for completion of ShutDown of Server
+        /// </summary>
+        /// <param name="serverUINotifier"></param>
+        /// <returns></returns>
+        public static Task StartTCPListener(IServerUINotifier serverUINotifier)
+        {
+            IAuthenticationService authenticationService = new FirebaseAuthService();
+            serverUINotifier.LogText("Initialized authentication service : " + authenticationService.Name);
+            ClientsManager clientsManager = new ClientsManager(serverUINotifier, authenticationService);
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, 2060);
+            tcpListener.Start();
+
+            Action listenForNewConnections = new Action(() =>
+            {
+                while (true)
+                {
+                    serverUINotifier.LogText("Listening to socket...");
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                    clientsManager.AcceptClient(tcpClient);
+                }
+            });
+
+            Task listenForNewConnectionsTask = new Task(listenForNewConnections);
+            listenForNewConnectionsTask.Start();
+            Task shutDownWaitTask = new Task(() => serverUINotifier.ServerWantsShutdown.WaitOne());
+            shutDownWaitTask.Start();
+
+            return Task.WhenAny(new List<Task>() { listenForNewConnectionsTask, shutDownWaitTask });
+            
+        }
+    }
+}
